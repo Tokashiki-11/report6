75f0d1f394ae0bdb1a54fe2c8425e05948f8b6c4
Tokashiki Anzu <e245744@ie.u-ryukyu.ac.jp>
2025-01-16 00:33:16 +0900

    Create Deck class

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e245744/Deck.java
@@ -0,0 +1,22 @@
+package jp.ac.uryukyu.ie.e245744;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+    private List<Card> cards;
+
+    public Deck() {
+        String[] suits = {"♥️", "♠️", "♣️", "♦️"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        cards = new ArrayList<>();
+
+        for (String suit : suits){
+            for (String rank : ranks){
+                cards.add(new Card(suit, rank));
+            }
+        }
+    }
+}
+
+
