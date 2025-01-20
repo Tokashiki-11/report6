e673f1d474f15948bdffcb3bd85a2319233d3c85
Tokashiki Anzu <e245744@ie.u-ryukyu.ac.jp>
2025-01-20 23:16:51 +0900

    Add JavaDoc to Main

--- app/src/main/java/jp/ac/uryukyu/ie/e245744/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245744/Main.java
@@ -2,22 +2,35 @@ package jp.ac.uryukyu.ie.e245744;
 
 import java.util.*;
 
+/**
+ * ゲーム進行を管理するメインクラス
+ * 2人のプレイヤーにカードを配り、任意のカードを交換する
+ */
 public class Main {
+    /**
+     * メインメソッド。ゲームの進行を制御
+     * 
+     * @param args 使用しない
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
        Deck deck = new Deck();
 
+       // プレイヤーの作成
        Player player1 = new Player("Taro");
        Player player2 = new Player("Jiro");
 
+       // 各プレイヤーに5枚のカードを配る
         for (int i = 0; i < 5; i++){
             player1.addCard(deck.draw());
             player2.addCard(deck.draw());
         }
 
+        // 手札を表示
         player1.showHand();
         player2.showHand();
 
+        // player1のターン(カード交換)
         System.out.println("Taroさんがカードを交換するターンです。");
         System.out.println("交換したいカードの番号を , 区切りで0~4で入力してください。交換しない場合はEnterを押してください。");
         String input1 = scanner.nextLine();
@@ -29,6 +42,7 @@ public class Main {
             player1.discardAndDraw(indices, deck);
         }
 
+        // player2のターン(カード交換)
         System.out.println("Jiroさんがカードを交換するターンです。");
         System.out.println("交換したいカードの番号を , 区切りで0~4で入力してください。交換しない場合はEnterを押してください。");
         String input2 = scanner.nextLine();
